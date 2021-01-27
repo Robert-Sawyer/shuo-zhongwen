7d2fa1070acbc27c5faede279874431005b702bb
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-27 20:39:54 +0100

    Utworzenie podstawowej struktury projektu + AuxComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './App.css';
+import Layout from "./components/UI/Layout/Layout";
 
 const App = props => {
 
     return (
         <div className="App">
-            <p>Shuo zhongwen</p>
+            <Layout>
+                <p>Shuo zhongwen</p>
+            </Layout>
         </div>
     );
 }
---
+++ src/components/UI/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Aux from "../../../hoc/AuxComponent/AuxComponent";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const Layout = props => {
+
+    return (
+        <Aux>
+            <Header/>
+            <main>{props.children()}</main>
+            <Footer/>
+        </Aux>
+    )
+}
+
+export default Layout
