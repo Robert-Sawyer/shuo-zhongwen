e1cb1d2f0d906008b2ad73c58055cd94ba8e2aaf
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-29 15:50:48 +0100

    Stworzenie struktury nagłówka

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import classes from './Header.module.css'
+import Logo from "../Logo/Logo";
+import HeaderNavigation from "../HeaderNavigation/HeaderNavigation";
 
 const Header = props => {
 
     return (
-        <header>
-
+        <header className={classes.Header}>
+            <div className={classes.Container}>
+                <div className={classes.Row}>
+                    <div className={classes.ColLeft}>
+                        <Logo/>
+                    </div>
+                    <div className={classes.ColRight}>
+                        <HeaderNavigation/>
+                    </div>
+                </div>
+            </div>
         </header>
     )
-
 }
 
 export default Header
