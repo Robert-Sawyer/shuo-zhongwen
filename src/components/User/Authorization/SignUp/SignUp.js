bc998a211e394148033d03badc445d13dc016e8f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-03-09 10:08:43 +0100

    Implementacja logiki w formularzu rejestracji + style

--- src/components/User/Authorization/SignUp/SignUp.js
+++ src/components/User/Authorization/SignUp/SignUp.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Link} from "react-router-dom"
+import {useSelector, useDispatch} from "react-redux"
 import classes from './SignUp.module.css'
 import Image from '../../../../assets/images/form1.jpg'
+import {userActions} from "../../../../store/action/user/auth"
+import Loader from "../../../UI/Loader/Loader"
 
 const SignUp = props => {
+
+    const [user, setUser] = useState({
+        // firstName: '',
+        username: '',
+        email: '',
+        password: ''
+    })
+    const [submitted, setSubmitted] = useState(false)
+    const registering = useSelector(state => state.signup.registering)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(userActions.logout())
+    }, [])
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setUser(user => ({...user, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setSubmitted(true)
+        if (user.username && user.email && user.password) {
+            dispatch(userActions.register(user))
+        }
+    }
 
     return (
         <section className={classes.SignupContainer}>
@@ -12,28 +44,43 @@ const SignUp = props => {
             <div className={classes.ContentBox}>
                 <div className={classes.FormBox}>
                     <h2>Rejestracja</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className={classes.InputBox}>
                             <span>Login</span>
-                            <input type="text" name=""/>
+                            <input type="text" name="username" value={user.username} onChange={handleChange}/>
+                            {
+                                submitted && !user.username &&
+                                <div className={classes.Invalid}>Login jest wymagany</div>
+                            }
                         </div>
                         <div className={classes.InputBox}>
                             <span>E-Mail</span>
-                            <input type="text" name=""/>
+                            <input type="text" name="email" value={user.email} onChange={handleChange}/>
+                            {
+                                submitted && !user.email &&
+                                <div className={classes.Invalid}>E-Mail jest wymagany</div>
+                            }
                         </div>
                         <div className={classes.InputBox}>
                             <span>Hasło</span>
-                            <input type="password" name=""/>
+                            <input type="password" name="password" value={user.password} onChange={handleChange}/>
+                            {
+                                submitted && !user.password &&
+                                <div className={classes.Invalid}>Hasło jest wymagane</div>
+                            }
                         </div>
                         <div className={classes.InputBox}>
                             <span>Powtórz hasło</span>
                             <input type="password" name=""/>
                         </div>
                         <div className={classes.InputBox}>
-                            <input type="submit" value="Zarejestruj" name=""/>
+                            <button type="submit" value="Zarejestruj" name="">
+                                Zarejestruj
+                                {registering && <Loader/>}
+                            </button>
                         </div>
                         <div className={classes.InputBox}>
-                            <p>Masz już konto? <a href="/login">Zaloguj się</a></p>
+                            <p>Masz już konto? <Link to="/login" className={classes.Link}>Zaloguj się</Link></p>
                         </div>
                     </form>
                 </div>
