2106e70893224f7498ce2216b219ab78c42f388b
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-03-09 10:58:16 +0100

    Dodanie logiki do formularza logowania + style

--- src/components/User/Authorization/Login/Login.js
+++ src/components/User/Authorization/Login/Login.js
@@ -1,8 +1,41 @@
-import React from 'react'
-import classes from "./Login.module.css";
-import Image from "../../../../assets/images/form1.jpg";
+import React, {useState, useEffect} from 'react'
+import {Link, useLocation} from "react-router-dom"
+import {useSelector, useDispatch} from "react-redux";
+import classes from "./Login.module.css"
+import Image from "../../../../assets/images/form1.jpg"
+import {userActions} from "../../../../store/action/user/auth";
+import Loader from "../../../UI/Loader/Loader";
 
 const Login = props => {
+
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: ''
+    })
+    const { username, password } = inputs
+    const [submitted, setSubmitted] = useState(false)
+    const loggingIn = useSelector(state => state.auth.loggingIn)
+    const dispatch = useDispatch()
+    const location = useLocation()
+
+    // useEffect(() => {
+    //     dispatch(userActions.logout());
+    // }, []);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setInputs(input => ({...input, [name]: value}))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setSubmitted(true)
+        if (username && password) {
+            const { from } = location.state || {from: {pathname: '/'}}
+            dispatch(userActions.login(username, password, from))
+        }
+    }
 
     return (
         <section className={classes.Container}>
@@ -12,23 +45,34 @@ const Login = props => {
             <div className={classes.ContentBox}>
                 <div className={classes.FormBox}>
                     <h2>Logowanie</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className={classes.InputBox}>
                             <span>Login</span>
-                            <input type="text" name=""/>
+                            <input type="text" name="username" value={username} onChange={handleChange}/>
+                            {
+                                submitted && !username &&
+                                <div className={classes.Invalid}>Login jest wymagany</div>
+                            }
                         </div>
                         <div className={classes.InputBox}>
                             <span>Hasło</span>
-                            <input type="password" name=""/>
+                            <input type="password" name="password" value={password} onChange={handleChange}/>
+                            {
+                                submitted && !password &&
+                                <div className={classes.Invalid}>Login jest wymagany</div>
+                            }
                         </div>
                         <div className={classes.Remember}>
                             <label><input type="checkbox" name=""/>Zapamiętaj mnie</label>
                         </div>
                         <div className={classes.InputBox}>
-                            <input type="submit" value="Zaloguj" name=""/>
+                            <button type="submit" value="Zaloguj" name="">
+                                Zaloguj
+                                {loggingIn && <Loader/>}
+                            </button>
                         </div>
                         <div className={classes.InputBox}>
-                            <p>Nie masz konta? <a href="/signup">Zarejestruj się</a></p>
+                            <p>Nie masz konta? <Link to="/signup" className={classes.Link}>Zarejestruj się</Link></p>
                         </div>
                     </form>
                 </div>
